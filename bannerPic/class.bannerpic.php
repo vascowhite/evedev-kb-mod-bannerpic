0dc94fae7ec0a2441261596e5b169c1cb5e11df5
vascowhite <vascowhite@gmail.com>
2011-07-16 22:43:12 +0000

    Initial upload

---
+++ bannerPic/class.bannerpic.php
@@ -0,0 +1,183 @@
+<?php
+require_once 'common/includes/class.kill.php';
+require_once 'common/includes/class.pilot.php';
+require_once 'common/includes/class.corp.php';
+require_once 'common/includes/class.comments.php';
+require_once 'common/includes/class.db.php';
+require_once 'common/includes/class.killlist.php';
+
+class BannerPic
+{
+	private $basepic = 'mods/bannerPic/base.jpg';
+	private $img;
+	private $kill;
+	private $font = 'mods/bannerPic/evesansmm.ttf';
+	private $sfont = 12;
+	private $lfont = 25;
+	private $owner;
+	private $ownerid = array();
+	private $cache;
+	private $nocache = false;
+	private $html = '';
+        private $noKills = true;
+	
+	public function __construct(){
+		
+		$kill_list = new KillList();
+		$kill_list->setOrdered(true);
+		$kill_list->setOrderBy('kll_timestamp DESC');
+                //$kill_list->setLimit(1);
+		$kill_list->setPodsNoobShips(Config::get('podnoobs'));
+                involved::load($kill_list);
+                while($kill = $kill_list->getKill()){
+                    if($kill->getFBCorpID() == CORP_ID){
+                        $this->kill = $kill;
+                        $killername = $this->kill->getFBPilotName();
+                        $kll_id = $this->kill->getID();
+                        $this->noKills = false;
+                        break;
+                    }
+                }
+		
+		//if this is already cached we don't need to go any further..
+		if(Config::get('mod_bannerpic_nocache') == 1) $this->nocache = true;
+		$bannerreplace = 0;
+		$bannerreplace = Config::get('mod_bannerpic_bannerreplace');
+		if($bannerreplace == 1) Config::set('style_banner', 'bannerpic.jpg');
+		$this->cache = KB_CACHEDIR . '/data/bannerpic' . $kll_id . '.jpg';
+		
+                $this->basepic = Config::get('mod_bannerpic_basepic');
+                
+		if(file_exists($this->cache) && !$this->nocache){
+			$this->img = imagecreatefromjpeg($this->cache);
+			if($bannerreplace == 1)	imagejpeg($this->img, 'banner/bannerpic.jpg', 100);
+			return;
+		}
+                
+		
+		//open base picture
+		$this->img = imagecreatefromjpeg($this->basepic);
+		$red = imagecolorallocate($this->img, 255, 0, 0);
+		
+                if($this->noKills){
+                    $killstr = "No kills yet - how sad!";
+                    imagefttext($this->img, $this->lfont, 0, 20, 30, $red, $this->font, $killstr);
+                    imagejpeg($this->img);
+                    imagedestroy($this->img);
+                    return;
+                }
+                
+		
+		//get victim info
+		$victimname = $this->kill->getVictimName();
+		$victimcorp = $this->kill->getVictimCorpName();
+		$victimalli = $this->kill->getVictimAllianceName();
+		$victimid = $this->kill->getVictimID();
+		$victim = new Pilot($victimid);
+		$victim->exists();
+		$victimid = $victim->getExternalID();
+		
+		//get the 64 x 64 victim portrait;
+		$victimgfile = 'http://image.eveonline.com/Character/' . $victimid . '_64.jpg';
+		$victimimg = imagecreatefromjpeg($victimgfile);
+		
+		$victimship = $this->kill->getVictimShip();
+		$victimshipname = $victimship->getName();
+		$url = 'http://image.eveonline.com/InventoryType/' . $victimship->getExternalID() . '_64.png'; 	
+		$victimshipimg = imagecreatefrompng($url);
+
+		//set variables for positions
+		//Config::get('mod_bannerpic_basepic') == null ? Config::set('mod_bannerpic_basepic', $this->basepic) : $this->basepic = Config::get('mod_bannerpic_basepic');
+		//victim picture
+		$vicpicLeft = 1;
+		Config::get('mod_bannerpic_vicpicleft') == null ? Config::set('mod_bannerpic_vicpicleft', $vicpicLeft) : $vicpicLeft = Config::get('mod_bannerpic_vicpicleft');
+		$vicpicTop = 1;
+		Config::get('mod_bannerpic_vicpictop') == null ? Config::set('mod_bannerpic_vicpictop', $vicpicTop) : $vicpicTop = Config::get('mod_bannerpic_vicpictop');
+		$vicpicTrans = 100;
+		Config::get('mod_bannerpic_vicpictrans') == null ? Config::set('mod_bannerpic_vicpictrans', $vicpicTrans) : $vicpicTrans = Config::get('mod_bannerpic_vicpictrans');
+		
+		//ship picture
+		$shippicLeft = 70;
+		Config::get('mod_bannerpic_shippicleft') == null ? Config::set('mod_bannerpic_shippicleft', $shippicLeft) : $shippicLeft = Config::get('mod_bannerpic_shippicleft');
+		$shippicTop = 1;
+		Config::get('mod_bannerpic_shippictop') == null ? Config::set('mod_bannerpic_shippictop', $shippicTop) : $shippicTop = Config::get('mod_bannerpic_shippictop');
+		$shippicTrans = 100;
+		Config::get('mod_bannerpic_shippictrans') == null ? Config::set('mod_bannerpic_shippictrans', $shippicTrans) : $shippicTrans = Config::get('mod_bannerpic_shippictrans');
+		//killer
+		$killstrLeft = 145;
+		Config::get('mod_bannerpic_killstrleft') == null ? Config::set('mod_bannerpic_killstrleft', $killstrLeft) : $killstrLeft = Config::get('mod_bannerpic_killstrleft');
+		$killstrTop = 15;
+		Config::get('mod_bannerpic_killstrtop') == null ? Config::set('mod_bannerpic_killstrtop', $killstrTop) : $killstrTop = Config::get('mod_bannerpic_killstrtop');
+		//victim corp
+		$corpstrLeft = 145;
+		Config::get('mod_bannerpic_corpstrleft') == null ? Config::set('mod_bannerpic_corpstrleft', $corpstrLeft) : $corpstrLeft = Config::get('mod_bannerpic_corpstrleft');
+		$corpstrTop = 30;
+		Config::get('mod_bannerpic_corpstrtop') == null ? Config::set('mod_bannerpic_corpstrtop', $corpstrTop) : $corpstrTop = Config::get('mod_bannerpic_corpstrtop');
+		//victim alliance
+		$allistrLeft = 145;
+		Config::get('mod_bannerpic_allistrleft') == null ? Config::set('mod_bannerpic_allistrleft', $allistrLeft) : $allistrLeft = Config::get('mod_bannerpic_allistrleft');
+		$allistrTop = 45;
+		Config::get('mod_bannerpic_allistrtop') == null ? Config::set('mod_bannerpic_allistrtop', $allistrTop) : $allistrTop = Config::get('mod_bannerpic_allistrtop');
+		//victim ship name
+		$shipstrLeft = 145;
+		Config::get('mod_bannerpic_shipstrleft') == null ? Config::set('mod_bannerpic_shipstrleft', $shipstrLeft) : $shipstrLeft = Config::get('mod_bannerpic_shipstrleft');
+		$shipstrTop = 60;
+		Config::get('mod_bannerpic_shipstrtop') == null ? Config::set('mod_bannerpic_shipstrtop', $shipstrTop) : $shipstrTop = Config::get('mod_bannerpic_shipstrtop');
+		//border box colours
+		$boxr = 186;
+		Config::get('mod_bannerpic_boxr') == null ? Config::set('mod_bannerpic_boxr', $boxr) : $boxr = Config::get('mod_bannerpic_boxr');
+		$boxg = 185;
+		Config::get('mod_bannerpic_boxg') == null ? Config::set('mod_bannerpic_boxg', $boxg) : $boxg = Config::get('mod_bannerpic_boxg');
+		$boxb = 183;
+		Config::get('mod_bannerpic_boxb') == null ? Config::set('mod_bannerpic_boxb', $boxb) : $boxb = Config::get('mod_bannerpic_boxb');
+		
+		//text colours
+		$textr = 255;
+		Config::get('mod_bannerpic_textr') == null ? Config::set('mod_bannerpic_textr', $textr) : $textr = Config::get('mod_bannerpic_textr');
+		$textg = 255;
+		Config::get('mod_bannerpic_textg') == null ? Config::set('mod_bannerpic_textg', $textg) : $textg = Config::get('mod_bannerpic_textg');
+		$textb = 255;
+		Config::get('mod_bannerpic_textb') == null ? Config::set('mod_bannerpic_textb', $textb) : $textb = Config::get('mod_bannerpic_textb');
+		
+		//font
+		Config::get('mod_bannerpic_fontfile') == null ? Config::set('mod_bannerpic_fontfile', $this->font) : $this->font = Config::get('mod_bannerpic_fontfile');
+		Config::get('mod_bannerpic_fontfile') == null ? Config::set('mod_bannerpic_fontfilesize', $this->sfont) : $this->sfont = Config::get('mod_bannerpic_fontsize');
+		
+		$killstr = "$victimname was killed by $killername";
+		$corpstr = "Corporation : $victimcorp";
+		$allistr = "Alliance : $victimalli";
+		$shipstr = "Ship : $victimshipname";
+		
+		$black = imagecolorallocate($this->img, 0, 0, 0);
+		$textcol = imagecolorallocate($this->img, $textr, $textg, $textb);
+		$box=imagecreate(68,68);
+		$bxcol=imagecolorallocate($box, $boxr, $boxg, $boxb);
+		
+		//pics to base pic
+		imagecopymerge($this->img, $box, $vicpicLeft, $vicpicTop, 0, 0, 68, 68, $vicpicTrans);
+		imagecopymerge($this->img,$victimimg,$vicpicLeft + 2, $vicpicTop + 2, 0, 0, 64, 64, $vicpicTrans);
+		imagecopymerge($this->img, $box, $shippicLeft, $shippicTop, 0, 0, 68, 68, 100);
+		imagecopymerge($this->img,$victimshipimg,$shippicLeft + 2, $shippicTop + 2, 0, 0, 64, 64, 100);
+		
+		//add text to image
+		imagefttext($this->img, $this->sfont, 0, $killstrLeft, $killstrTop, $textcol, $this->font, $killstr);
+		imagefttext($this->img, $this->sfont, 0, $corpstrLeft, $corpstrTop, $textcol, $this->font, $corpstr);
+		imagefttext($this->img, $this->sfont, 0, $allistrLeft, $allistrTop, $textcol, $this->font, $allistr);
+		imagefttext($this->img, $this->sfont, 0, $shipstrLeft, $shipstrTop, $textcol, $this->font, $shipstr);
+		
+		//cache the file
+		imagejpeg($this->img, $this->cache, 100);
+		
+		//if the board banner option is checked then put a copy in the banners dir.
+		if($bannerreplace == 1) imagejpeg($this->img, 'banner/bannerpic.jpg', 100);
+		
+		//destroy the images
+		imagedestroy($box);
+		imagedestroy($victimimg);
+		imagedestroy($victimshipimg);
+	}
+	
+	public function generate(){
+		return $this->img;
+	}
+}
---
+++ bannerPic/init.php
@@ -0,0 +1,267 @@
+<?php
+include_once 'class.bannerpic.php';
+event::register('home_assembling', 'banner::replace');
+
+class banner
+{	
+	function replace(){
+		if(Config::get('mod_bannerpic_bannerreplace') == 1){
+			$banner = new BannerPic();
+			$banner->generate();
+		}
+	}
+	
+	function basePic(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_basepic'])){
+			Config::set('mod_bannerpic_basepic', $_POST['option_mod_bannerpic_basepic']);
+		}
+		$basepic = 'mods/bannerPic/base.jpg';
+		Config::get('mod_bannerpic_basepic') == null ? Config::set('mod_bannerpic_basepic', $basepic) : $basepic = Config::get('mod_bannerpic_basepic');
+		$html = "<input type='text' id='option_mod_bannerpic_basepic' name='option_mod_bannerpic_basepic' value='$basepic' size='". strlen($basepic) . "' />";
+		return $html;
+	}
+	
+	function vicPicLeft(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_vicpicleft'])){
+			Config::set('mod_bannerpic_vicpicleft', $_POST['option_mod_bannerpic_vicpicleft']);
+		}
+		$vicpicLeft = 1;
+		Config::get('mod_bannerpic_vicpicleft') == null ? Config::set('mod_bannerpic_vicpicleft', $vicpicLeft) : $vicpicLeft = Config::get('mod_bannerpic_vicpicleft');
+		$html = "<input type='text' id='option_mod_bannerpic_vicpicleft' name='option_mod_bannerpic_vicpicleft' value='$vicpicLeft' />";
+		return $html;
+	}
+	
+	function vicPicTop(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_vicpictop'])){
+			Config::set('mod_bannerpic_vicpictop', $_POST['option_mod_bannerpic_vicpictop']);
+		}
+		$vicpicTop = 1;
+		Config::get('mod_bannerpic_vicpictop') == null ? Config::set('mod_bannerpic_vicpictop', $vicpicTop) : $vicpicTop = Config::get('mod_bannerpic_vicpictop');
+		$html = "<input type='text' id='option_mod_bannerpic_vicpictop' name='option_mod_bannerpic_vicpictop' value='$vicpicTop' />";
+		return $html;
+	}
+	
+	function vicPicTrans(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_vicpictrans'])){
+			Config::set('mod_bannerpic_vicpictrans', $_POST['option_mod_bannerpic_vicpictrans']);
+		}
+		$vicpicTrans = 100;
+		Config::get('mod_bannerpic_vicpictrans') == null ? Config::set('mod_bannerpic_vicpictrans', $vicpicTrans) : $vicpicTrans = Config::get('mod_bannerpic_vicpictrans');
+		$html = "<input type='text' id='option_mod_bannerpic_vicpictrans' name='option_mod_bannerpic_vicpictrans' value='$vicpicTrans' />";
+		return $html;
+	}
+	
+	function shipPicLeft(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_shippicleft'])){
+			Config::set('mod_bannerpic_shippicleft', $_POST['option_mod_bannerpic_shippicleft']);
+		}
+		$shippicLeft = 70;
+		Config::get('mod_bannerpic_shippicleft') == null ? Config::set('mod_bannerpic_shippicleft', $shippicLeft) : $shippicLeft = Config::get('mod_bannerpic_shippicleft');
+		$html = "<input type='text' id='option_mod_bannerpic_shippicleft' name='option_mod_bannerpic_shippicleft' value='$shippicLeft' />";
+		return $html;
+	}
+	
+	function shipPicTop(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_shippictop'])){
+			Config::set('mod_bannerpic_shippictop', $_POST['option_mod_bannerpic_shippictop']);
+		}
+		$shippicTop = 1;
+		Config::get('mod_bannerpic_shippictop') == null ? Config::set('mod_bannerpic_shippictop', $shippicTop) : $shippicTop = Config::get('mod_bannerpic_shippictop');
+		$html = "<input type='text' id='option_mod_bannerpic_shippictop' name='option_mod_bannerpic_shippictop' value='$shippicTop' />";
+		return $html;
+	}
+	
+	function shipPicTrans(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_shippictrans'])){
+			Config::set('mod_bannerpic_shippictrans', $_POST['option_mod_bannerpic_shippictrans']);
+		}
+		$shippicTrans = 100;
+		Config::get('mod_bannerpic_shippictrans') == null ? Config::set('mod_bannerpic_shippictrans', $shippicTrans) : $shippicTrans = Config::get('mod_bannerpic_shippictrans');
+		$html = "<input type='text' id='option_mod_bannerpic_shippictrans' name='option_mod_bannerpic_shippictrans' value='$shippicTrans' />";
+		return $html;
+	}
+	
+	function killStrLeft(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_killstrleft'])){
+			Config::set('mod_bannerpic_killstrleft', $_POST['option_mod_bannerpic_killstrleft']);
+		}
+		$killstrLeft = 145;
+		Config::get('mod_bannerpic_killstrleft') == null ? Config::set('mod_bannerpic_killstrleft', $killstrLeft) : $killstrLeft = Config::get('mod_bannerpic_killstrleft');
+		$html = "<input type='text' id='option_mod_bannerpic_killstrleft' name='option_mod_bannerpic_killstrleft' value='$killstrLeft' />";
+		return $html;
+	}
+	
+	function killStrTop(){
+		$html = '';
+		if(isset($_POST['option_mod_bannerpic_killstrtop'])){
+			Config::set('mod_bannerpic_killstrtop', $_POST['option_mod_bannerpic_killstrtop']);
+		}
+		$killstrTop = 15;
+		Config::get('mod_bannerpic_killstrtop') == null ? Config::set('mod_bannerpic_killstrtop', $killstrTop) : $killstrTop = Config::get('mod_bannerpic_killstrtop');
+		$html = "<input type='text' id='option_mod_bannerpic_killstrtop' name='option_mod_bannerpic_killstrtop' value='$killstrTop' />";
+		return $html;
+	}
+	
+	function corpStrLeft(){
+		$html = '';
+		$option = 'mod_bannerpic_corpstrleft';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 145;
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function corpStrTop(){
+		$html = '';
+		$option = 'mod_bannerpic_corpstrtop';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 30;
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function alliStrLeft(){
+		$html = '';
+		$option = 'mod_bannerpic_allistrleft';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 145;
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function alliStrTop(){
+		$html = '';
+		$option = 'mod_bannerpic_allistrtop';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 45;
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function shipStrLeft(){
+		$html = '';
+		$option = 'mod_bannerpic_shipstrleft';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 145;
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function shipStrTop(){
+		$html = '';
+		$option = 'mod_bannerpic_shipstrtop';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 60;
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function fontFile(){
+		$html = '';
+		$option = 'mod_bannerpic_fontfile';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 'mods/bannerPic/evesansmm.ttf';
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function fontSize(){
+		$html = '';
+		$option = 'mod_bannerpic_fontsize';
+		if(isset($_POST['option_' . $option])){
+			Config::set($option, $_POST['option_'. $option]);
+		}
+		$default = 12;
+		Config::get($option) == null ? Config::set($option, $default) : $default = Config::get($option);
+		$html = "<input type='text' id='option_$option' name='option_$option' value='$default' />";
+		return $html;
+	}
+	
+	function boxCols(){
+		$html = '';
+		$option = 'mod_bannerpic_box';
+		if(isset($_POST['option_' . $option . 'r'])){
+			Config::set($option .'r', $_POST['option_'. $option . 'r']);
+		}
+		if(isset($_POST['option_' . $option . 'g'])){
+			Config::set($option . 'g', $_POST['option_'. $option . 'g']);
+		}
+		if(isset($_POST['option_' . $option . 'b'])){
+			Config::set($option . 'b', $_POST['option_'. $option . 'b']);
+		}
+		$default = 185;
+		Config::get("{$option}r") == null ? Config::set("{$option}r", $default) : $default = Config::get("{$option}r");
+		$html = "Red : <input type='text' id='option_{$option}r' name='option_{$option}r' value='$default' />";
+		Config::get("{$option}g") == null ? Config::set("{$option}g", $default) : $default = Config::get("{$option}g");
+		$html .= " Green : <input type='text' id='option_{$option}g' name='option_{$option}g' value='$default' />";
+		Config::get("{$option}b") == null ? Config::set("{$option}b", $default) : $default = Config::get("{$option}b");
+		$html .= " Blue : <input type='text' id='option_{$option}b' name='option_{$option}b' value='$default' />";
+		return $html;
+	}
+	
+function textCols(){
+		$html = '';
+		$option = 'mod_bannerpic_text';
+		if(isset($_POST['option_' . $option . 'r'])){
+			Config::set($option .'r', $_POST['option_'. $option . 'r']);
+		}
+		if(isset($_POST['option_' . $option . 'g'])){
+			Config::set($option . 'g', $_POST['option_'. $option . 'g']);
+		}
+		if(isset($_POST['option_' . $option . 'b'])){
+			Config::set($option . 'b', $_POST['option_'. $option . 'b']);
+		}
+		$default = 255;
+		Config::get("{$option}r") == null ? Config::set("{$option}r", $default) : $default = Config::get("{$option}r");
+		$html = "Red : <input type='text' id='option_{$option}r' name='option_{$option}r' value='$default' />";
+		Config::get("{$option}g") == null ? Config::set("{$option}g", $default) : $default = Config::get("{$option}g");
+		$html .= " Green : <input type='text' id='option_{$option}g' name='option_{$option}g' value='$default' />";
+		Config::get("{$option}b") == null ? Config::set("{$option}b", $default) : $default = Config::get("{$option}b");
+		$html .= " Blue : <input type='text' id='option_{$option}b' name='option_{$option}b' value='$default' />";
+		return $html;
+	}
+	
+	function preview(){
+		$html = '';
+		$basepic = Config::get('mod_bannerpic_basepic');
+		$html = "<img src='?a=bannerPic' width='100%' />";
+		return $html;
+	}
+	
+	function about(){
+	  	$html = '<p>';
+	  	$html .= "v1.11 by vasco di (Paul White 2010)";
+	  	$html .= '</p>';
+	  	$html .= "<p>Detailed instructions,support and updates are available on the <a href='http://eve-id.net/forum/viewtopic.php?f=505&t=17007'>EVE Development Network</a></p>";
+	  	return $html;
+  }
+}
